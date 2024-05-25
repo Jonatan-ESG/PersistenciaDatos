5a9043081eb6eff28402b0ef3d0e0d9bab03480f
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-05-25 16:09:12 -0600

    :construction_worker: Se agregó el método eliminar producto

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
     Crear, Leer, Actualizar y Eliminar
 */
 
-import { crearProducto } from './mantenimientos/producto/mantenimiento_productos'
+import { crearProducto, eliminarProducto } from './mantenimientos/producto/mantenimiento_productos'
 
-crearProducto({ nombre: 'Chocolate Hershey', precio: 10, stock: 1 })
+// crearProducto({ nombre: 'Chocolate Hershey', precio: 10, stock: 1 })
+eliminarProducto(2)
--- src/mantenimientos/producto/mantenimiento_productos.ts
+++ src/mantenimientos/producto/mantenimiento_productos.ts
@@ -1,17 +1,25 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { CrearProducto, Producto } from './producto.interface'
 
+const direccionArchivo = './src/datos/productos.json'
+
 const lecturaProductos = (): Producto[] => {
-    const archivo: string = readFileSync('./src/datos/productos.json', 'utf8')
+    const archivo: string = readFileSync(direccionArchivo, 'utf8')
     if (archivo.trim() === '') return []
     return JSON.parse(archivo) as Producto[]
 }
 
 const crearProducto = (crearProducto: CrearProducto) => {
     const productosActuales = lecturaProductos()
-    crearProducto.id_producto = 4
+    crearProducto.id_producto = 4 // Agregar lógica para deteminar el siguiente ID
     productosActuales.push(crearProducto as Producto)
-    writeFileSync('./src/datos/productos.json', JSON.stringify(productosActuales))
+    writeFileSync(direccionArchivo, JSON.stringify(productosActuales))
 }
 
-export { lecturaProductos, crearProducto }
+const eliminarProducto = (id_producto: number) => {
+    const productosActuales = lecturaProductos()
+    const productosFinales = productosActuales.filter((producto) => producto.id_producto !== id_producto)
+    writeFileSync(direccionArchivo, JSON.stringify(productosFinales))
+}
+
+export { lecturaProductos, crearProducto, eliminarProducto }
